7f418daaca8fcf4cf948adb6ddd94e6d1d8e782b
steffijerome0809 <steffi.jerome@yahoo.com>
2020-09-19 19:02:21 -0400

    installed seq package and set up server js and added express js package

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Quiz-App
+
 Node and Sequelize
+npm init -y
+npm install
+server.js
+install npm express
+npm install sequelize (dont use npm i sequelize)
---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+
+const PORT = process.env.PORT || 8080;
+
+const db = require('./models');
+
+//required API and HTML routes
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+//creating an express app and configuring middleware
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+//sequelize connection
+
+db.sequelize.sync.then(() => {
+  app.listen(PORT, () => {
+    console.log(`server listening on port ${PORT}`);
+  });
+});
