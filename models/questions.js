ed86a5429ee379941f753c8b71315fac2c9ff531
steffijerome0809 <steffi.jerome@yahoo.com>
2020-09-20 00:56:33 -0400

    added model for questions

---
+++ models/questions.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+  const Questions = sequelize.define('Questions', {
+    question: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    choices: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    answer: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return Questions;
+};
