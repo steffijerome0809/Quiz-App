7156879d097bd45c7a572717b28adc3e36ae3bde
steffijerome0809 <steffi.jerome@yahoo.com>
2020-09-21 19:16:23 -0400

    clear highscores

--- public/assets/js/highscore.js
+++ public/assets/js/highscore.js
@@ -1,1 +1,10 @@
-var clearhighscores = $("#clearBtn");
+var clearhighscores = $('#clearBtn');
+
+var tbody = $('#tbody');
+for (let i = 0; i < localStorage.length; i++) {
+  var userName = localStorage.key(i);
+  var userScore = localStorage.getItem(userName);
+  tbody.prepend(
+    '<tr class="scores"><td>' + userName + ' - ' + userScore + '</td></tr>'
+  );
+}
