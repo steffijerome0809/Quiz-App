a031ade0b87a50bdd1cd1980205e88b7b1475094
steffijerome0809 <steffi.jerome@yahoo.com>
2020-09-20 23:58:34 -0400

    changed button styling to show and hide

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -9,19 +9,17 @@ $(document).ready(() => {
   function quizStart() {
     timeLeft = 75;
     startTimer();
-    // createQuestion();
+    createQuestion();
   }
 
   // function changes timer display every showTime (second)
   function startTimer() {
- 
     timer.text(timeLeft);
     quizTime = setInterval(showTime, 1000);
   }
 
   //  function equates a showTime to a second and determines when timer reaches zero
   function showTime() {
-    
     if (timeLeft !== 0) {
       timeLeft--;
       timer.text(timeLeft);
@@ -32,6 +30,14 @@ $(document).ready(() => {
     return;
   }
 
+  //  function hides initial elements and shows quiz relevant ones, then starts main quiz function
+  function createQuestion() {
+    console.log('code here');
+
+    $('.codeQuiz').hide();
+    $('.quiz').show();
+    //CodeQuiz(qNumber);
+  }
   // Document => Start Quiz
   $(document).on('click', '#startbutton', () => {
     quizStart();
